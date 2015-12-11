c7a0764070b2c5ff2f9c0d06418c8cdb963fd38a
Tim Habermaas <tim.habermaas@gmail.com>
2015-12-11 01:59:36 +0100

    Day 01

---
+++ app/Advent01.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Data.List (findIndex)
+import Data.Maybe (fromJust)
+
+bracketToNumber :: Char -> Int
+bracketToNumber '(' = 1
+bracketToNumber ')' = -1
+bracketToNumber _ = 0
+
+floors :: String -> [Int]
+floors = reverse . snd . foldl (\(sum, acc) s -> (sum + bracketToNumber s, (sum + bracketToNumber s) : acc)) (0, [])
+
+firstBasementPosition :: String -> Maybe Int
+firstBasementPosition = findIndex (< 0) . floors
+
+lastFloor :: String -> Int
+lastFloor = last . floors
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ show $ lastFloor input
+  putStrLn $ show $ (fromJust $ firstBasementPosition input) + 1
